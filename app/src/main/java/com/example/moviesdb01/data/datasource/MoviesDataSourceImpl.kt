55fd0d25c9e123d70ba1f480e47fd062c1212483
adenilsonricardo <85114978+adenilsonricardo@users.noreply.github.com>
2022-03-31 17:43:26 -0300

    Removido chave da API

--- app/src/main/java/com/example/moviesdb01/data/datasource/MoviesDataSourceImpl.kt
+++ app/src/main/java/com/example/moviesdb01/data/datasource/MoviesDataSourceImpl.kt
@@ -1,11 +1,12 @@
 package com.example.moviesdb01.data.datasource
 
+import com.example.moviesdb01.BuildConfig
 import com.example.moviesdb01.data.api.MoviesApi
 import com.example.moviesdb01.data.models.MoviesResponse
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
-const val API_KEY = "cf20abfa4daa8cc80b1708476db3178d"
+const val API_KEY = "Sua_Chave_da_API_aqui"
 
 class MoviesDataSourceImpl(private val service: MoviesApi): MoviesDataSource  {
 
